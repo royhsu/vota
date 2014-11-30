ae5579dc8d399e16667ca28fb34ba8c22c9bc010
Roy Hsu <appleday@me.com>
2014-11-30 18:18:35 +0800

    add magicalrecord

---
+++ vota-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  vota-Bridging-Header.h
+//  vota
+//
+//  Created by 許郁棋 on 2014/11/30.
+//  Copyright (c) 2014年 tinyworld. All rights reserved.
+//
+
+#define MR_SHORTHAND
+#import <CoreData+MagicalRecord.h>
